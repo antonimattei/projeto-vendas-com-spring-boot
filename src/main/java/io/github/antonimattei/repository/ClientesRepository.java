24c227a77ec728c579431e6bcff2cfab27d6d81f
Antoni Mattei <antonidemedeiros@gmail.com>
2022-01-06 21:30:47 -0300

    Project Started.

---
+++ src/main/java/io/github/antonimattei/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.antonimattei.repository;
+
+import io.github.antonimattei.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persistir(Cliente cliente) {
+        //Acessa a base e salva o cliente.
+    }
+}
